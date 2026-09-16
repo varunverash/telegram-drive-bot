cd8f6740256c7da7b2f2270928f6852cf4088027
varunverash <varunverash@gmail.com>
2026-09-16 18:55:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3498,7 +3498,7 @@ async def post_init(app):
 
         "jobs": {},
 
-        "large_sem": asyncio.Semaphore(3),
+        "large_sem": asyncio.Semaphore(4),
 
         
         "small_sem": asyncio.Semaphore(
